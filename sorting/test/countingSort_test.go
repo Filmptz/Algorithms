d68fc97232ea5d0a1d05ca1ac8998f7d973d5cf4
Filmptz <you@example.com>
2021-06-29 16:10:46 +0700

    Change function name

--- sorting/test/countingSort_test.go
+++ sorting/test/countingSort_test.go
@@ -6,9 +6,9 @@ import (
 	"sort"
 )
 
-var slice = []int{1,2,3,4,5,4,3,2,1,3,4}
-func TestSliceIsSorted(t *testing.T){
-	sorted := sorting.CountingSort(slice)
+var slice_counting = []int{1,2,3,4,5,4,3,2,1,3,4}
+func TestCountingSort(t *testing.T){
+	sorted := sorting.CountingSort(slice_counting)
 	res1 := sort.SliceIsSorted(sorted, 
 		func(i, j int) bool { 
 		return sorted[i] < sorted[j] 
